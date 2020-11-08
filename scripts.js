c163bd95c82984976b2101182f4aabccc82e57c7
Lital Israel <lital400@gmail.com>
2020-11-08 14:04:57 -0500

    JS Validation

---
+++ scripts.js
@@ -0,0 +1,23 @@
+[ 'datePicker', 'dateAvailable' ].forEach(function( id ) {     // set default date to today's date
+
+    document.getElementById( id ).valueAsDate = new Date(); 
+
+});
+
+document.getElementById('dateAvailable').min = new Date().toISOString().split('T')[0];   // minimum date is today's date
+
+
+function validateForm() {
+    var ssn1, ssn2, ssn3, text;
+  
+    ssn1 = document.forms["myForm"]["ssn-1"].value;
+    ssn2 = document.forms["myForm"]["ssn-2"].value;
+    ssn3 = document.forms["myForm"]["ssn-3"].value;
+
+    if (isNaN(ssn1) || isNaN(ssn2) || isNaN(ssn3))    // validate SSN
+    { 
+        document.getElementById("validate").innerHTML = "Social Security number must contain numbers only";
+        document.getElementById('ssn').scrollIntoView();
+        return false;
+    } 
+  }
