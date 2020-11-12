3ae151a8c2f8bd521cf7240abf4d2d24e69ce254
Lital Israel <lital400@gmail.com>
2020-11-12 09:18:29 -0500

    small changes

--- scripts.js
+++ scripts.js
@@ -8,7 +8,7 @@ document.getElementById('dateAvailable').min = new Date().toISOString().split('T
 
 
 function validateForm() {
-    var ssn1, ssn2, ssn3, text;
+    var ssn1, ssn2, ssn3;
   
     ssn1 = document.forms["myForm"]["ssn-1"].value;
     ssn2 = document.forms["myForm"]["ssn-2"].value;
@@ -19,7 +19,7 @@ function validateForm() {
         document.getElementById("validate").innerHTML = "Social Security number must contain numbers only";
         document.getElementById('ssn').scrollIntoView();
         return false;
-    } 
+    }
     else
     {
         alert("Your application has been successfully submitted");
@@ -28,9 +28,9 @@ function validateForm() {
 
   //Makes all the month fields have a default date of today
 const monthControl = document.querySelectorAll('input[type="month"]');
-const date= new Date()
-const month=("0" + (date.getMonth() + 1)).slice(-2)
-const year=date.getFullYear()
+const date = new Date();
+const month = ("0" + (date.getMonth() + 1)).slice(-2);
+const year = date.getFullYear();
 
 monthControl.forEach((e) => {
     e.value = `${year}-${month}`;
